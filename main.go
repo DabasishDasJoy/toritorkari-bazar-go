3c667dff1dbaca38cdce4ef47dfeb47a34834a69
Dabasish Das Joy <dabasishdasjoy92@gmail.com>
2024-12-25 01:55:06 +0600

    updated route

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	e := echo.New()
-	e.GET("/health", func(c echo.Context) error {
+	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "OK")
 	})
 	cmd.Serve(e)
