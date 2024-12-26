3993050c1b99d4176df5b2f1580f0ec448f246bb
Dabasish Das Joy <dabasishdasjoy92@gmail.com>
2024-12-26 21:54:55 +0600

    refactored products routes

--- internal/http/routes/product.go
+++ internal/http/routes/product.go
@@ -2,14 +2,12 @@ package routes
 
 import (
 	"toritorkari-bazar/internal/http/controllers"
-	middleware "toritorkari-bazar/internal/http/middlewares"
 
 	"github.com/labstack/echo/v4"
 )
 
 func ProductRouts(e *echo.Echo, productController *controllers.ProductController) {
 	Product := e.Group("product")
-	Product.Use(middleware.AuthenticateUser())
 	Product.POST("", productController.CreateProducts)
-	Product.GET("/list", productController.GetProducts)
+	Product.GET("", productController.GetProducts)
 }
